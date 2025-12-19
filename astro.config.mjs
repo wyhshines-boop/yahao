df70f0094fd2ebc63e29d0e6e3e042e1619ac94d
wyhshines-boop <wyhshines@gmail.com>
2025-12-19 20:13:47 +0800

    网站地图

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'zh'],
-        routing: {
-            prefixDefaultLocale: false
-        }
+  site: 'https://yahaotranslate.com',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'zh'],
+    routing: {
+      prefixDefaultLocale: false
     }
+  },
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-US',
+          zh: 'zh-CN',
+        },
+      },
+    })
+  ]
 });
